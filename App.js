b13631473081a7c87258bc0d92c05e44b8f4d389
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-11 15:18:48 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "react-navigation";
+
+import LoginScreen from "./screens/LoginScreen";
+
+export default class App extends React.Component {
+  render() {
+    return <AppStackNavigator />;
+  }
+}
+
+const AppStackNavigator = createStackNavigator({
+  Login: {
+    screen: LoginScreen
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
